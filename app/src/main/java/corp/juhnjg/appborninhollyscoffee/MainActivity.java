661feab3a41fa57a64f9fb040d97270ad69357f1
Jeon <juhnjg@gmail.com>
2019-05-12 15:45:18 +0900

    swipe delete the note

--- app/src/main/java/corp/juhnjg/appborninhollyscoffee/MainActivity.java
+++ app/src/main/java/corp/juhnjg/appborninhollyscoffee/MainActivity.java
@@ -1,11 +1,13 @@
 package corp.juhnjg.appborninhollyscoffee;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.util.Log;
 import android.view.View;
 
@@ -61,6 +63,7 @@ public class MainActivity extends AppCompatActivity implements NotesRecyclerAdap
         mRecyclerView.setLayoutManager(linearLayoutManager);
         VerticalSpacingItemDecorator itemDecorator = new VerticalSpacingItemDecorator(10);
         mRecyclerView.addItemDecoration(itemDecorator);
+        new ItemTouchHelper(itemTouchHelperCallback).attachToRecyclerView(mRecyclerView);
         mNotesRecyclerAdapter = new NotesRecyclerAdapter(mNotes, this);
         mRecyclerView.setAdapter(mNotesRecyclerAdapter);
     }
@@ -80,4 +83,23 @@ public class MainActivity extends AppCompatActivity implements NotesRecyclerAdap
         Intent intent = new Intent(this, NoteActivity.class);
         startActivity(intent);
     }
+
+    private void deleteNote(Note note){
+        mNotes.remove(note);
+        mNotesRecyclerAdapter.notifyDataSetChanged();
+    }
+
+    private ItemTouchHelper.SimpleCallback itemTouchHelperCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT) {
+        @Override
+        public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder viewHolder1) {
+            return false;
+        }
+
+        @Override
+        public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+            deleteNote(mNotes.get(viewHolder.getAdapterPosition()));
+        }
+    };
+
+
 }
